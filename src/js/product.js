2932d9edd634ff7fbb56aa4260a6cfc919c74ea8
Deana Briggs <145316150+deanabriggs@users.noreply.github.com>
2025-01-18 07:46:55 -0800

    fixed addProductToCart function

--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,17 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  const cart = getLocalStorage("so-cart") || []; // retrieve current cart info from local storage or make an empty list
+  const exists = cart.some((cartItem) => cartItem.Id === product.Id); // check if the product is already in the cart
+
+  // if the product isn't already in the cart, add it, otherwise do nothing
+  if (!exists) {
+    cart.push(product);
+    setLocalStorage("so-cart", cart);
+  }
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await findProductById(e.target.dataset.id);
