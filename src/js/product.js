83bb47cc11362ff66b23d5fa08d96d391d9ea6d2
ItsMen <mvdo1091@gmail.com>
2025-01-18 19:55:47 -0500

    commit for ind activity 01 - fix add item to cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,27 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // Retrieve the cart data from local storage
+  let cart = getLocalStorage("so-cart");
+
+  if (cart) {
+    // If the cart exists and is an array, add the new product
+    if (Array.isArray(cart)) {
+      cart.push(product);
+    } else {
+      // If the cart contains a single item, convert it to an array and add the new product
+      cart = [cart, product];
+    }
+  } else {
+    // If no cart exists, create a new array with the product
+    cart = [product]
+  }
+
+  // Save the updated cart back to local storage
+  setLocalStorage("so-cart", cart);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await findProductById(e.target.dataset.id);
